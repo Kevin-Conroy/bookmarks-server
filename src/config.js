4f116581d19d1195fa453acc96383885ca2e8a15
Kevin Conroy <Kcdrumming@gmail.com>
2021-05-24 10:46:34 -0700

    5/24 commit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
+  API_TOKEN: process.env.API_TOKEN || 'a0ed4e75-a14f-444c-9128-ca35ce697e72',
   DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
 }
